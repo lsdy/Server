fe459bf02a34f5f8fa1dfc8337644b8efcd90170
jacket <jacket.1995@qq.com>
2018-01-24 17:32:48 +0800

    去掉文件注释

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,3 @@
-/** 
- * @file         main.cpp
- * @brief
- * @details
- * @author       Pengxiang Jin
- * @date     	2018年1月23日
- * @version  	Version
- * @par Copyright (c):
- *       桂林优利特医疗电子有限公司
- * @par History:
- *   version: author, date, desc\n
- */
 
 #include <boost/asio.hpp>
 #include <boost/thread.hpp>
